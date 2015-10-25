e80a640f4f3575aaceea098d577434e1e7ea3901
Kamps-Duac <kamps.duac@gmail.com>
2015-10-25 16:13:23 -0400

    finished knockout

--- js/catdata.js
+++ js/catdata.js
@@ -1,8 +1,5 @@
-
-// Model
-var data = {
-	currentCat: null,
-	cats: [{
+var initialCats = [
+	{
 		id: 1,
 		name: "Beach Cat",
 		catImgSrc: "img/Beach Cat.jpg",
@@ -17,192 +14,51 @@ var data = {
 		name: "Professor Cat",
 		catImgSrc: "img/Professor Cat.jpg",
 		clicks: 0
-	}]
-};
-
-// Controller
-var controller = {
-	init: function() {
-		controller.setCurrentCat(data.cats[0]);
-
-		catImgView.init();
-		catListView.init();
-		catAdminView.init();
-	},
-
-	getCurrentCat: function() {
-		return data.currentCat;
-	},
-
-	getCats: function() {
-		return data.cats;
-	},
-
-	setCurrentCat: function(cat){
-		data.currentCat = cat;
-	},
-
-	addOneClick: function() {
-		data.currentCat.clicks++;
-	},
-
-	refreshViews: function() {
-		catImgView.render();
-		catAdminView.render();
 	}
-};
+]
 
-// View
-var catImgView = {
-	init: function() {
-		this.clickCounterElem = $("#clkCount");
-		this.catListElem = $("#catList");
-		this.catImageElem = $("img#selectedCat");
-		this.catNameElem = $("clkPicName");
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clicks);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.catImgSrc);
 
-		// Add click event for Cat Picture
-		this.catImageElem.click(function() {
-			controller.addOneClick();
-			controller.refreshViews();
-		});
-		this.catImageElem.mousedown(function() {
-			$(this).css("opacity", 0.5);
-		});
-		this.catImageElem.mouseup(function() {
-			$(this).css("opacity", 1);
-		});
-
-		// Render the view on load
-		this.render();
-	},
-
-	render: function() {
-		// Update the DOM elements with Current Cat data
-		var currentCat = controller.getCurrentCat();
-		if (currentCat !== null){
-			this.clickCounterElem.text(currentCat.clicks);
-			this.catNameElem.text(currentCat.name);
-			this.catImageElem.attr("src",currentCat.catImgSrc);
-			$("#clickCountDisplay").show();
-			$("#catList>li").removeClass("active");
+	this.level = ko.computed(function() {
+		var clicks = this.clickCount();
+		if (clicks <= 5) {
+			return "Infant";
+		} else if (clicks <= 20) {
+			return "Child";
+		} else if (clicks <= 50) {
+			return "Teen"
+		} else {
+			return "Adult"
 		}
-		
-	}
-};
+	}, this);
+}
 
-var catListView = {
-	init: function() {
-		this.catListElem = $("#catList");
-		//this.currentCat = data.cats[1];
-		this.render();
-	},
+var ViewModel = function() {
 
-	render: function() {
-		var cat, liElem, aElem, i;
+	var self = this;
 
-		var cats = controller.getCats();
+	this.catList = ko.observableArray([]);
 
-		function generateCatClickHandler(catCopy) {
-			return function() {
-				controller.setCurrentCat(catCopy);
-				catImgView.render();
-				catListView.catListElem
-					.children().children().removeClass("btn-success");
-				$(this).addClass("btn-success");
+	initialCats.forEach(function(catItem){
+		self.catList.push( new Cat(catItem) );
+	});
 
-				controller.refreshViews();
-			};
-		}
+	this.currentCat = ko.observable( this.catList()[0] );
 
-		for (i = 0; i < cats.length; i++) {
-			cat = cats[i];
+	this.setCurrentCat = function(cat){
+		self.currentCat(cat);
+		console.log("setCurrentCat was called " + cat.name())
+		//console.log("current cat is " + currentCat());
+		console.log("self current cat is " + self.currentCat().name())
+	};
 
-			// Here create nested element <li><a></a></li>
-			liElem = document.createElement('li');
-			aElem = document.createElement('a');
-			$(aElem).addClass("btn btn-primary btn-sm");
-			aElem.textContent = cat.name;
-
-			aElem.addEventListener('click', (generateCatClickHandler(cat)));
-			liElem.appendChild(aElem);
-			this.catListElem.append(liElem);
-		}
-	}
-};
-
-var catAdminView = {
-	init: function() {
-		this.catAdminBtn = $("#catAdminBtn");
-		this.catNameFld = $("#catNameFld");
-		this.catImgUrlFld = $("#catImgUrlFld");
-		this.catClickCounterFld = $("#catClickCounterFld");
-
-		this.catAdminBtn.click(function() {
-			$("#adminForm").toggle();
-		});
-
-		this.render();
-	},
-
-	render: function() {
-		var currentCat = controller.getCurrentCat();
-
-		this.catNameFld.val(currentCat.name);
-		this.catImgUrlFld.val(currentCat.catImgSrc);
-		this.catClickCounterFld.val(currentCat.clicks);
-	}
-};
-
-controller.init();
-
-// for (var i = 0; i < cats.length; i++) {
-// 	$("#catList").append(
-// 		"<li>" + '<a class="btn btn-primary btn-xs active"' +
-// 		'role="button" data-catid=' +
-// 		cats[i].id + ">" +
-// 		cats[i].name +
-// 		"</a>" + "</li>")
-// }
-
-
-
-// // Controller
-// $("#catList > li > a").click(function() {
-// 	var selectedCatId = $(this).data("catid");
-// 	$("#clickCountDisplay").show();
-// 	for (var i = 0; i < cats.length; i++) {
-// 		var cat = cats[i];
-// 		if (cat.id == selectedCatId) {
-// 			cat.clicks += 1;
-// 			var catImgSrc = cat.src;
-// 			$("img#selectedCat").attr("src", "img/" + catImgSrc);
-// 			$("img#selectedCat").data("catid", cat.id);
-// 			$("#clkPicName").html(cat.name);
-// 			$("#clkCount").html(cat.clicks);
-// 		}
-// 	}
-// })
-
-// // Can refector some here
-// $("img#selectedCat").click(function() {
-// 	var catid = $(this).data("catid");
-// 	for (var i = 0; i < cats.length; i++) {
-// 		var cat = cats[i];
-// 		if (cat.id == catid) {
-// 			cat.clicks += 1;
-// 			view.clickCounterElem.html(cat.clicks);
-// 		}
-// 	}
-// })
-
-function generateCatClickHandler(catCopy) {
-	return function() {
-		controller.setCurrentCat(catCopy);
-		catImgView.render();
+	this.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+		console.log("Incremented Counter");
 	};
 }
 
-// for (i = 0; i < cats.length; i++) {
-// 	cat = cats[i];
-// 	elem.addEventListener('click', (generateCatClickHandler(cat)));
-// }
+ko.applyBindings(new ViewModel());
