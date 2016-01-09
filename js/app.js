56ad1b200ecb99247749c1aec9a45b6d3aba69c5
Kamps-Duac <kamps.duac@gmail.com>
2016-01-09 11:36:48 -0500

    preping for archive branch

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('projectGuac', [
+    'ngRoute',
+    'projectGuac.main'
+]).
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({
+        redirectTo: '/main'
+    });
+}]);
