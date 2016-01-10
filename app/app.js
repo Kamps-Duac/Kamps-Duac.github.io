e6030a2b86b6ac38c3f470815974d85d9348a89d
Kamps-Duac <kamps.duac@gmail.com>
2016-01-10 15:49:53 -0500

    redesign with simple angular architecture and more modern layout

--- app/app.js
+++ app/app.js
@@ -1,13 +1,30 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('projectGuac', [
-  'ngRoute',
-  'projectGuac.projectListView',
-  'projectGuac.blogListView'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  otherwise({
-  	redirectTo: '/projects'});
-}]);
+// // Declare app level module which depends on views, and components
+// angular.module('projectGuac', [
+//     'ngRoute',
+//     'projectGuac.projectListView',
+//     'projectGuac.blogListView'
+// ]).
+// config(['$routeProvider', function($routeProvider) {
+//     $routeProvider.
+//     otherwise({
+//         redirectTo: '/projects'
+//     });
+// }]);
+
+var projectGuacApp = angular.module('projectGuac', ['ngRoute']);
+
+projectGuacApp.controller('MainCtrl', ['$scope',
+    function($scope) {
+        $scope.pageLocation = "Home";
+
+        $scope.currentYear = new Date().getFullYear();
+    }
+]);
+
+// projectGuacApp.controller('ProjectListCtrl', ['$scope',
+//     function($scope) {
+//         $scope.test = "Project Scope";
+//     }
+// ]);
---
+++ app/js/controllers-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('projectGuac.MainCtrl module', function() {
+
+  beforeEach(module('projectGuac.MainCtrl'));
+
+  describe('MainCtrl controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var mainCtrl = $controller('MainCtrl');
+      expect(mainCtrl).toBeDefined();
+    }));
+
+  });
+});
