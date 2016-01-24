5153ddebd4d7b02581c5bb01196991b275c1bcc5
Kamps-Duac <kamps.duac@gmail.com>
2016-01-24 13:23:34 -0500

    yeoman_angular with grunt and bootswatch-paper

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kampsduacApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kampsduacApp
+ */
+angular.module('kampsduacApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
