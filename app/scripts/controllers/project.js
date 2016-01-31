cc11a3f45347353470a12cbf4e800ed0a3e31016
Kamps-Duac <kamps.duac@gmail.com>
2016-01-31 11:32:50 -0500

    updating projects and some formatting

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -11,10 +11,16 @@ angular.module('kampsduacApp')
     .controller('ProjectCtrl', function($scope) {
         $scope.projects = [{
             'name': 'Cat Clicker',
-            'desc': 'Vanilla Javascript appliction with that counts cat clicks. Built throughout Udacity\'s Javascript design patterns course.',
+            'desc': 'Vanilla Javascript appliction with that counts cat clicks. Built through Udacity\'s Javascript design patterns course.',
             'img_src': 'images/cat.jpg',
-            'source': 'https://github.com/Kamps-Duac/Kamps-Duac.github.io/tree/cat_click_vanilla',
-            'demo': 'https://github.com/Kamps-Duac/Kamps-Duac.github.io/tree/cat_click_vanilla'
+            'source': 'https://github.com/Kamps-Duac/cat-clicker',
+            'demo': 'https://kamps-duac.github.io/cat-clicker/'
+        }, {
+            'name': 'Kording',
+            'desc': 'Simple Musical Chord reference app.',
+            'img_src': 'images/kording-logo.png',
+            'source': 'https://github.com/Kamps-Duac/kording',
+            'demo': 'https://kamps-duac.github.io/kording/'
         }, {
             'name': 'Mapacious',
             'desc': 'Playing around with Goole Maps API and Yelp',
